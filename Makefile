09c77917f18002f694e13689955e477e7b49a761
Matthew Millman <inaxeon@hotmail.com>
2021-10-14 12:29:41 +0000

    Added guidsub tool

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 EDK2_TOOLS = $(PWD)/edk2/BaseTools/Source/C/bin
 BUILD = $(PWD)/build
+TOOLS = $(PWD)/tools
 SRC = $(PWD)/src
 GENFFS = $(EDK2_TOOLS)/GenFfs
 GENSEC = $(EDK2_TOOLS)/GenSec
 PKGBUILD = $(PWD)/edk2/Build/MdeModule/RELEASE_GCC5/X64/src
+PCIBUSBUILD = $(PWD)/edk2/Build/MdeModule/RELEASE_GCC5/X64/MdeModulePkg/Bus/Pci/PciBusDxe/PciBusDxe/OUTPUT
 
 .EXPORT_ALL_VARIABLES:
 	EDK_TOOLS_PATH = $(PWD)/edk2/BaseTools
@@ -11,7 +13,10 @@ PKGBUILD = $(PWD)/edk2/Build/MdeModule/RELEASE_GCC5/X64/src
 default: 
 	@echo "Available targets:"
 
-linux-edk2-base:
+$(TOOLS)/guidsub:
+	gcc $(dir $@)/guidsub.c -o $@ -luuid
+
+linux-edk2-base: $(TOOLS)/guidsub
 	cd edk2 && git submodule update --init
 	cd edk2 && bash -c '. edksetup.sh BaseTools'
 	echo "ACTIVE_PLATFORM       = MdeModulePkg/MdeModulePkg.dsc" > edk2/Conf/target.txt
@@ -26,6 +31,14 @@ linux-edk2:
 	cd edk2 && patch -p1 --forward < ../patches/edk2-with-serial-debug.patch || true
 	cd edk2 && bash -c '. edksetup.sh BaseTools && build'
 
+
+$(BUILD)/PciBusDxe.ffs:
+	mkdir -p build
+	$(GENSEC) -o $(basename $@).sec_ui -s EFI_SECTION_USER_INTERFACE -n "`cat $(PCIBUSBUILD)/$(basename $(notdir $@)).inf | grep BASE_NAME | cut -f 2 -d '=' | xargs`"
+	$(GENSEC) $(PCIBUSBUILD)/$(basename $(notdir $@)).efi -s EFI_SECTION_PE32 -o $(basename $@).sec_pe32
+	$(GENSEC) -s EFI_SECTION_COMPRESSION -c PI_STD $(basename $@).sec_pe32 $(basename $@).sec_ui -o $(basename $@).sec_compressed
+	$(GENFFS) -i $(basename $@).sec_compressed -t EFI_FV_FILETYPE_DRIVER -g `cat $(PCIBUSBUILD)/$(basename $(notdir $@)).inf | grep FILE_GUID | cut -f 2 -d '=' | xargs` -s -o $(basename $@).ffs
+
 $(BUILD)/%.ffs: linux-edk2
 	mkdir -p build
 	$(GENSEC) -s EFI_SECTION_DXE_DEPEX $(PKGBUILD)/$(basename $(notdir $@))/$(basename $(notdir $@))/OUTPUT/$(basename $(notdir $@)).depex -o $(basename $@).sec_depex
@@ -34,6 +47,6 @@ $(BUILD)/%.ffs: linux-edk2
 	$(GENSEC) -s EFI_SECTION_COMPRESSION -c PI_STD $(basename $@).sec_pe32 $(basename $@).sec_ui -o $(basename $@).sec_compressed
 	$(GENFFS) -i $(basename $@).sec_depex -i $(basename $@).sec_compressed -t EFI_FV_FILETYPE_DRIVER -g `cat $(SRC)/$(basename $(notdir $@))/$(basename $(notdir $@)).inf | grep FILE_GUID | cut -f 2 -d '=' | xargs` -s -o $(basename $@).ffs
 
-linux-efi-ffs: $(BUILD)/PciHotPlug.ffs $(BUILD)/PciDxeShim.ffs
+linux-efi-ffs: $(BUILD)/PciHotPlug.ffs $(BUILD)/PciDxeShim.ffs $(BUILD)/PciBusDxe.ffs
 
 clean: 
---
+++ tools/guidsub.c
@@ -0,0 +1,165 @@
+/*
+ *   File:   guidsub.c
+ *   Author: Matthew Millman (inaxeon@hotmail.com)
+ *
+ *   Tool for substituting little endian GUIDs in binary files
+ *
+ *   This is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 2 of the License, or
+ *   (at your option) any later version.
+ *   This software is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <uuid/uuid.h>
+
+#define SWAP32(x) ((x >> 24) & 0xFF) | ((x << 8) & 0xFF0000) | ((x >> 8) & 0xFF00) | ((x << 24) & 0xFF000000)
+#define SWAP16(x) ((x << 8) | ((x >> 8) & 0xFF))
+
+void uuid_fix_endianness(uuid_t uuid);
+
+int main(int argc, char *argv[])
+{
+    FILE *f = NULL;
+    size_t fileSize = 0;
+    int pos = 0;
+    uint8_t *buffer = NULL;
+    const char *fileNameStr = NULL;
+    const char *originalGuidStr = NULL;
+    const char *newGuidStr = NULL;
+    int opt = 0;
+    bool substituted = false;
+
+    uuid_t originalGuid;
+    uuid_t newGuid;
+
+    while ((opt = getopt(argc, argv, "f:o:n:?")) != -1)
+    {
+        switch (opt)
+        {
+        case 'f':
+            fileNameStr = optarg;
+            break;
+        case 'o':
+            originalGuidStr = optarg;
+            break;
+        case 'n':
+            newGuidStr = optarg;
+            break;
+        default:
+            fprintf(stderr, "\r\nUsage: %s -f <filename> -o <original GUID> -n <new GUID>\n\n", argv[0]);
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (fileNameStr == NULL)
+    {
+        fprintf(stderr, "Error: No filename specified\n");
+        return EXIT_FAILURE;
+    }
+
+    if (originalGuidStr == NULL)
+    {
+        fprintf(stderr, "Error: Original GUID not specified\n");
+        return EXIT_FAILURE;
+    }
+
+    if (newGuidStr == NULL)
+    {
+        fprintf(stderr, "Error: New GUID not specified\n");
+        return EXIT_FAILURE;
+    }
+
+    if (uuid_parse(originalGuidStr, originalGuid))
+    {
+        fprintf(stderr, "Error: Failed to parse original GUID\n");
+        return EXIT_FAILURE;
+    }
+
+    uuid_fix_endianness(originalGuid);
+
+    if (uuid_parse(newGuidStr, newGuid))
+    {
+        fprintf(stderr, "Error: Failed to parse new GUID\n");
+        return EXIT_FAILURE;
+    }
+
+    uuid_fix_endianness(newGuid);
+
+    f = fopen(fileNameStr, "rb");
+
+    if (!f)
+    {
+        fprintf(stderr, "Error: Failed to open input file\n");
+        return EXIT_FAILURE;
+    }
+
+    fseek(f, 0, SEEK_END);
+    fileSize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    buffer = malloc(fileSize);
+
+    if (fread(buffer, fileSize, 1, f) != 1)
+    {
+        fprintf(stderr, "Error: Failed to read input file\n");
+        return EXIT_FAILURE;
+    }
+
+    fclose(f);
+
+    for (pos = 0; pos < (fileSize - sizeof(uuid_t)); pos++)
+    {
+        if (memcmp((buffer + pos), originalGuid, sizeof(uuid_t)) == 0)
+        {
+            memcpy((buffer + pos), newGuid, sizeof(uuid_t));
+            substituted = true;
+        }
+    }
+
+    if (!substituted)
+    {
+        fprintf(stdout, "GUID not found in source file. Exiting.\n");
+        return EXIT_FAILURE;
+    }
+
+    f = fopen(fileNameStr, "wb");
+
+    if (!f)
+    {
+        fprintf(stderr, "Error: Failed to open file for writing\n");
+        return EXIT_FAILURE;
+    }
+
+    if (fwrite(buffer, fileSize, 1, f) != 1)
+    {
+        fprintf(stderr, "Error: Failed to write updated file\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("Successfully updated protocol GUID\n");
+
+    fclose(f);
+    free(buffer);
+
+    return EXIT_SUCCESS;
+}
+
+void uuid_fix_endianness(uuid_t uuid)
+{
+    // ffs. really?
+    *(uint32_t *)(&uuid[0]) = SWAP32(*(uint32_t *)(&uuid[0]));
+    *(uint16_t *)(&uuid[4]) = SWAP16(*(uint16_t *)(&uuid[4]));
+    *(uint16_t *)(&uuid[6]) = SWAP16(*(uint16_t *)(&uuid[6]));
+}
